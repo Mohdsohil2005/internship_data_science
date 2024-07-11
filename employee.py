8cc522911239fe44776869c2195954ce4002f158
MOHD SOHIL <142766946+Mohdsohil2005@users.noreply.github.com>
2024-07-11 11:35:42 +0530

    Add files via upload
    Module in python

---
+++ employee.py
@@ -0,0 +1,27 @@
+import manager
+
+print(manager.rohit_id)
+print(manager.rohit_pass)
+
+print(manager.eployee_list)
+
+# calling
+manager.rohit_hii()
+
+import manager as mng
+print(mng.rohit_id)
+print(mng.rohit_pass)
+
+print(mng.eployee_list)
+
+import manager as mng
+print(mng.manager_name)
+print(mng.manager_id)
+print(mng.manager_pass)
+
+from manager import rohit_id,rohit_pass
+from manager import rohit_hii
+rohit_hii()
+print()
+print(rohit_pass)
+print(rohit_id)
---
+++ manager.py
@@ -0,0 +1,24 @@
+#<<<<<<<<<<<<<< Modual >>>>>>>>>>>>>>>>
+
+manager_name = "Dharmpal ji"
+manager_address = "Bihar"
+
+eployee_list = ['rohit','rahul','mohit','suresh','jagdessh'] 
+
+rohit_id = 'hello@gmail.com'
+rohit_pass = "124@786"
+
+def rohit_hii():
+    print("Employee name : ","Rohit")
+    print('Rohit id : ',rohit_id)
+    print('ROhit password : ',rohit_pass)
+
+if __name__ == "__mani__":
+
+    def manager_hii():
+        print("Manager name : ","Dharmpal")
+        print('Dharmpal id : ',manager_id)
+        print('Dharmpal password : ',manager_pass)
+
+    manager_id = 'manager_boss@gmail.com'
+    manager_pass = 'boss123456'
